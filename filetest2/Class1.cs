2053d31137d4c176d34e52662e26d80ffeb91836
cheolwook <loky123@naver.com>
2022-05-21 14:29:22 +0900

    file 추가

---
+++ filetest2/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file_test2
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 1;
+            Console.WriteLine(10.0 / y);
+            Console.WriteLine(x / y);
+        }
+    }
+}
